1cf55c0cf79a13290a560c1d4c46f02e6f6ad73f
Josh Hathcock <jdspiral@gmail.com>
2014-04-19 14:57:35 -0400

    app with blog and mail

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jdapp::Application.config.secret_key_base = '3a96db0d34a6c4e82b28b749eb7051bde66a1c0aaade1b65cad0f628ca05dd64a91c40562e3103e79389fa2854990182eb907ba2f39acc0272fe7ae8c1fb0096'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jdapp::Application.config.session_store :cookie_store, key: '_jdapp_session'
