c9d0bc58ec2dace3a8e325bb5a2e2fdb725b7610
Josh Hathcock <jdspiral@gmail.com>
2014-04-19 16:04:43 -0400

    setup mandrill_delivery.rb

--- app/models/mandrill_delivery.rb
+++ app/models/mandrill_delivery.rb
@@ -1,17 +1,17 @@
-require 'mandrill'  
-m = Mandrill::API.new
+require 'mandrill'
+mandrill = Mandrill::API.new ENV["MANDRILL_PASSWORD"]
 message = {  
  :subject=> "Hello from the Mandrill API",  
  :from_name=> "Your name",  
  :text=>"Hi message, how are you?",  
  :to=>[  
    {  
-     :email=> "recipient@theirdomain.com",  
+     :email=> "jdspiral@gmail.com",  
      :name=> "Recipient1"  
    }  
  ],  
  :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",  
- :from_email=>"sender@yourdomain.com"  
+ :from_email=>"jdspiral@gmail.com"  
 }  
 sending = m.messages.send message  
 puts sending
