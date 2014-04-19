47de2ccee0d690697ff7b0afefa21d0b49842453
Josh Hathcock <jdspiral@gmail.com>
2014-04-19 15:59:52 -0400

    setup mandrill_delivery.rb

--- app/models/mandrill_delivery.rb
+++ app/models/mandrill_delivery.rb
@@ -1,60 +1,17 @@
-require 'mandrill'
-
-class MandrillDelivery
-  attr_accessor :message
-
-  def initialize(mail)
-  end
-
-  def deliver!(mail)
-    build_meta_mandrill(mail)
-    send_mandrill(mail)
-  end
-
-#Message stuff
-
-  def build_meta_mandrill(mail)
-
-    #build Mandrill message hash
-    @message = {
-      :from_name=> "Admin",
-      :from_email=>"admin@somewhere.com",
-      :subject=> "#{mail['subject']}",
-      :to=>[
-            {
-              :email=> "#{mail['to']}",
-              :name=> "#{mail['name']}"
-            }
-           ],
-      :auto_text => true,
-      :global_merge_vars => [
-                             {
-                               :name => "LISTCOMPANY",
-                               :content => "Company Name Here"
-                             }
-                            ]
-    }
-
-    true
-  end
-
-  #sends email via Mandrill
-  def send_mandrill(mail)
-    m = Mandrill::API.new
-
-    sending = m.messages.send_template('Sign-Up Confirmation',
-                                       [
-                                        {
-                                          :name => 'main',
-                                          :content => "#{mail.body}"
-                                        },
-                                        {
-                                          :name => 'unsub',
-                                          :content => "#{mail['unsub_link']}"
-                                        }
-                                       ],
-                                       message = @message)
-    Rails.logger.info sending
-  end
-
-end
+require 'mandrill'  
+m = Mandrill::API.new
+message = {  
+ :subject=> "Hello from the Mandrill API",  
+ :from_name=> "Your name",  
+ :text=>"Hi message, how are you?",  
+ :to=>[  
+   {  
+     :email=> "recipient@theirdomain.com",  
+     :name=> "Recipient1"  
+   }  
+ ],  
+ :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",  
+ :from_email=>"sender@yourdomain.com"  
+}  
+sending = m.messages.send message  
+puts sending
