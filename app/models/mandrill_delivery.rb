a4ab478752d2638033f6e3fda9bbdf207fb5985b
Josh Hathcock <jdspiral@gmail.com>
2014-04-19 19:12:00 -0400

    added proper username and email variables to json

--- app/models/mandrill_delivery.rb
+++ app/models/mandrill_delivery.rb
@@ -48,8 +48,8 @@ class MandrillDelivery
  :text=>"Thanks for signing up to my mailing list! This message is sent to you via Mandrill's API :)",  
  :to=>[  
    {  
-     :email=> "jdspiral@gmail.com",  
-     :name=> "yoyoyoy"  
+     :email=> "@user.email",  
+     :name=> "@user.name"  
    }  
  ],  
  :from_email=>"jdspiral@gmail.com"  
