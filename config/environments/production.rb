1a9793627c61fb847925a3db005a5f8e488d90ec
Josh Hathcock <jdspiral@gmail.com>
2014-04-19 22:08:43 -0400

    added proper username and email variables to json

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,8 @@ Jdapp::Application.configure do
   # Email Settings
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'jdspiral@gmail.com'}
+  #config.action_mailer.default_options = {from: 'jdspiral@gmail.com'}
+  config.mandrill_mailer.default_url_options = { :host => 'http://peaceful-forest-3344.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp #:mandrill_delivery
   # config.action_mailer.smtp_settings = {
   # address:              'smtp.mandrillapp.com',
