8084b2e1a15b66623015b9ba89e260018131e62a
Josh Hathcock <jdspiral@gmail.com>
2014-04-19 20:24:33 -0400

    added proper username and email variables to json

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
   	@user = user
 
-  	mail( :to => @user.email, 
-  	:name => @user.name,
-  	:subject=> "Welcome!")
+  	mail(to: @user.email, 
+  	name: @user.name,
+  	subject: "Welcome!")
   end
 end
