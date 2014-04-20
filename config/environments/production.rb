dbbac7ba11a41df75ebe4b21f9e9d913ecc3386d
Josh Hathcock <jdspiral@gmail.com>
2014-04-20 12:57:34 -0400

    remove mandrill api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Jdapp::Application.configure do
   # Email Settings
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_options = {from: 'jdspiral@gmail.com', host: 'http://peaceful-forest-3344.herokuapp.com/'}
+  config.action_mailer.default_options = {from: 'jdspiral@gmail.com'}
   config.action_mailer.delivery_method = :smtp #:mandrill_delivery
   config.action_mailer.smtp_settings = {
   address:              'smtp.mandrillapp.com',
