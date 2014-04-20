e974ba5f6219640b742b202feaefe883febd55a6
Josh Hathcock <jdspiral@gmail.com>
2014-04-19 23:55:51 -0400

    fixed migrate problem

--- app/models/mandrill_delivery.rb
+++ app/models/mandrill_delivery.rb
@@ -1,23 +1,4 @@
 require 'mandrill'  
-m = Mandrill::API.new
-message = {  
- :subject=> "Hello from the Mandrill API",  
- :from_name=> "Your name",  
- :text=>"Hi message, how are you?",  
- :to=>[  
-   {  
-     :email=> "recipient@theirdomain.com",  
-     :name=> "Recipient1"  
-   }  
- ],  
- :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",  
- :from_email=>"sender@yourdomain.com"  
-}  
-sending = m.messages.send message  
-puts sending
-
-rendered = m.templates.render 'MyTemplate', [{:name => 'sign-up-notification', :content => 'The main content block'}]
-puts rendered['html'] # print out the rendered HTML
 
 class MandrillDelivery
   attr_accessor :message
@@ -67,8 +48,8 @@ class MandrillDelivery
  :text=>"Thanks for signing up to my mailing list! This message is sent to you via Mandrill's API :)", 
  :to=>[  
    {  
-     :email=> user_name,  
-     :name=>   
+     :email=> user_email,  
+     :name=>  user_name 
    }  
  ], 
  :from_email=>"jdspiral@gmail.com"  
