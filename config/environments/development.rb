0ae895c2e944cfa95a7ccb9777eea3185365e82a
Josh Hathcock <jdspiral@gmail.com>
2014-05-04 01:08:32 -0400

    added blog info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Jdapp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -26,17 +26,15 @@ Jdapp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'jdspiral@gmail.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.mandrillapp.com',
-  port:                 587,
-  domain:               'localhost:3000',
-  user_name:            ENV['MANDRILL_USERNAME'],
-  password:             ENV['MANDRILL_APIKEY'],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  config.mandrill_mailer.default_url_options = { :host => 'localhost:3000'}
+  # config.action_mailer.default_options = {from: 'jdspiral@gmail.com'}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  # address:              'smtp.mandrillapp.com',
+  # port:                 587,
+  # domain:               'localhost:3000',
+  # user_name:            ENV['MANDRILL_USERNAME'],
+  # password:             ENV['MANDRILL_APIKEY'],
+  # authentication:       'plain',
+  # enable_starttls_auto: true  }
 end
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,1 +1,5 @@
 # ActionMailer::Base.add_delivery_method :mandrill_delivery, MandrillDelivery
+
+MandrillMailer.configure do |config|
+  config.api_key = ENV['MANDRILL_APIKEY']
+end
