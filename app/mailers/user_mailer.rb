303b4118a3db61cff5baad12b2ba207f43a21a2f
jdspiral <jdspiral@gmail.com>
2014-10-16 18:47:04 -0400

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < MandrillMailer::TemplateMailer
 
   def welcome_email(user)
   	mandrill_mail template: 'sign-up-notification',
-	subject: "Jdapp Has a Gift for You!",
+	subject: "Thanks for Signing Up!!",
 	from_name: "Josh Hathcock",
 	to: {email: user.email, name: user.name},
 	vars: { 
